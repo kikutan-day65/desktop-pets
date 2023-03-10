293bcae61f97da9805115d9ffda23934ac365401
Haruto Mori <baystarsman@outlook.jp>
2023-03-10 19:37:21 +0100

    set some default variables and path

---
+++ main.py
@@ -0,0 +1,14 @@
+import random   # a pet is moving in random motion
+import tkinter
+import pyautogui
+
+x = 1400
+cycle = 0
+check = 1
+idle_num = [1, 2, 3, 4]
+sleep_num = [10, 11, 12, 13, 15]
+walk_left = [6, 7]
+walk_right = [8, 9]
+event_number = random.randrange(1, 3, 1)
+impath = './images/'    # set the image path here
+
